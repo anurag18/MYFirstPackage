bc8950dd92085f18893b9c8dc735826c14341cc8
Anurag Sharma <anutheriddle@gmail.com>
2023-05-25 08:19:03 +0530

    deleted test files

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "MYFirstPackage",
             dependencies: []),
-        .testTarget(
-            name: "MYFirstPackageTests",
-            dependencies: ["MYFirstPackage"]),
     ]
 )

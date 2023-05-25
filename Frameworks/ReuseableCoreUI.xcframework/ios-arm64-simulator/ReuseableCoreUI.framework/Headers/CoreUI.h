ce8bee97a7e8f40465ec37b10ea9d777dd19f220
Anurag Sharma <anutheriddle@gmail.com>
2023-05-25 09:55:08 +0530

    updated the XCFramework

---
+++ Frameworks/ReuseableCoreUI.xcframework/ios-arm64-simulator/ReuseableCoreUI.framework/Headers/CoreUI.h
@@ -0,0 +1,18 @@
+//
+//  CoreUI.h
+//  CoreUI
+//
+//  Created by Anurag Sharma on 21/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreUI.
+FOUNDATION_EXPORT double CoreUIVersionNumber;
+
+//! Project version string for CoreUI.
+FOUNDATION_EXPORT const unsigned char CoreUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreUI/PublicHeader.h>
+
+
--- Package.swift
+++ Package.swift
@@ -7,9 +7,10 @@ let package = Package(
     name: "MYFirstPackage",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
+        
         .library(
             name: "MYFirstPackage",
-            targets: ["MYFirstPackage"])
+            targets: ["MYFirstPackage", "ReuseableCoreUI"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.

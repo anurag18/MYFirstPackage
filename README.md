41537b797bb3bea2dd8840929b55340102e046cc
Anurag Sharma <anutheriddle@gmail.com>
2023-05-25 08:14:14 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MYFirstPackage
+
+A description of this package.
---
+++ Sources/MYFirstPackage/MYFirstPackage.swift
@@ -0,0 +1,6 @@
+public struct MYFirstPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
